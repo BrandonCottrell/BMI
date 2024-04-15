a9175748f6e1b8e083870da2381e99b845eac5ac
brand <brandoncottrell54@gmail.com>
2024-04-14 19:13:16 -0500

    Initial Commit

---
+++ BMI/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace BMI;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    
+    
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        double weight = Convert.ToInt32(weight_int.Text);
+        double height = Convert.ToInt32(height_int.Text);
+        double bmi = (weight / (height * height)) * 703;
+        BMI.Text = $"Your BMI is {bmi}. ";
+    }
+}
